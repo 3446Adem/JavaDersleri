35d500240e3df6efbd3d60aa15085f9c6ef5360f
Adem <cenkyavuz2022@gmail.com>
2022-12-26 15:05:45 +0300

    git commit -m 'commit mesajı gönderme şekli'

---
+++ operators.js
@@ -0,0 +1,47 @@
+console.log("*** operators****")
+
+const cola = 20
+const gumm = 5
+const biscuits = 15
+
+const totalPrice = cola + gumm + biscuits
+//  *totalPrice hata
+
+console.log("total price:", totalPrice)
+
+console.log("total price:", +totalPrice)
+
+const comments = totalPrice + " TL"
+console.log(comments)
+ const firstName = "Adem"
+ const lastName = "OKUMUŞ"
+ console.log( firstName + " "+lastName)
+
+ const s1 = 5 , s2 = "4" , s3 = "three" , s4 = 8
+ console.log(s1+s2)
+ console.log(s1-s2)
+ console.log(s1-s3)
+ console.log(s4-s1)
+ const difference = s1 - s2
+ console.log(difference)
+ const dif = s1 -s3 
+ console.log(dif)
+ console.log(isNaN(dif))
+ console.log(isNaN(difference))
+
+ const yearOfBirth = 2017
+  
+ const name = "Jane"
+ console.log(name + " is " + (2022-yearOfBirth) + " years old")
+ console.log(name + " is " + (new Date().getFullYear()-yearOfBirth) + " years old")
+ const date = new Date
+ console.log(date)
+ `benim sayım ${5}'dir.`
+ ``
+kkgfkjgj
+
+
+
+
+
+
